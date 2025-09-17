6c7ae9b5d4f382c3a79382fb58e55ef325824769
truscgm <truscgm@pedarecc.sa.edu.au>
2025-09-17 03:33:54 +0000

    Menu printing

--- Beef_Burger_Co.py
+++ Beef_Burger_Co.py
@@ -1,7 +1,37 @@
-beef_burgers = {
-    'Cheeseburger': 12.50, 'Beef Burger': 16.50, 'Schlumberger': 17.60, 'Triple Bacon Artery Clogger': 19.99
-}
+beef_burgers = {'Cheeseburger': 12.50, 'Beef Burger': 16.50, 'Schlumberger': 17.60, 'Triple Bacon Artery Clogger': 19.99}
+drinks = {'Large Coca Cola': }
 
+cheeseburger = ['Cheese', 'Pickles', 'Onion', 'Ketchup']
+schlumberger = ['']
+
+customer_order = []
+
+def order():
+    print('')
+    print('--- Beef Burger Menu ---')
+    print('1. Burger')
+    print('2. Drink')
+    print('3. View/edit order')
+    print('4. Checkout')
+    print('')
+    user_input = input('What would customer like to do? ')
+    if user_input == '1':
+        for burger in beef_burgers:
+            print(f'{burger}: ${beef_burgers[burger]:.2f}')
+    elif user_input == '2':
+        print('2')
+    elif user_input == '3':
+        print('3')
+    elif user_input == '4':
+        print('4')
+    else:
+        print('Invalid input')
+        order()
+order()
+
+
+
+'''
 total_order = []      
 total_order_prices = []   
 
@@ -28,3 +58,4 @@ print('Welcome to the Beef Burger Co. menu!')
 menu()
 customer_order()
 check_out()
+'''
